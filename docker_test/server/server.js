cc19c962a303a946e4a76b667f0080dd93b122f8
Parkpoom Tirasak <parkpoom2984@gmail.com>
2018-12-17 10:47:37 +0700

    add local branch

--- docker_test/server/server.js
+++ docker_test/server/server.js
@@ -3,7 +3,7 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
 var noteTest = require('../routes/note_test')
-
+const env = process.env.NODE_ENV
 var app = module.exports = loopback();
 
 app.start = function() {
@@ -11,7 +11,7 @@ app.start = function() {
   return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
+    console.log(`env: ${env}, Web server listening at: ${baseUrl}`);
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
