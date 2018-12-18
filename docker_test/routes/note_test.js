9d8b9702754e882d5285084b579f21ccf60ec2ab
Parkpoom Tirasak <parkpoom2984@gmail.com>
2018-12-18 10:58:49 +0700

    test commit

--- docker_test/routes/note_test.js
+++ docker_test/routes/note_test.js
@@ -3,7 +3,7 @@ const router = loopback.Router()
 
 router.get('/test_example',
   function (req, res) {
-    res.status(200).json({'user_id': 'test'})
+    res.status(200).json({'user_id': 'test_example'})
   }
 )
 
