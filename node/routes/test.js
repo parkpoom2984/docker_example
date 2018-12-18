c0fa84030df0e4316497631f56a37e1ae97eeb45
Parkpoom Tirasak <parkpoom2984@gmail.com>
2018-12-18 11:55:29 +0700

    change config file to local env

--- node/routes/test.js
+++ node/routes/test.js
@@ -3,8 +3,8 @@ const router = loopback.Router()
 
 router.get('/test',
   function (req, res) {
-    console.log(`/test_example_dev`)
-    res.status(200).json({'message': 'test_example_dev'})
+    console.log(`/test_example_local`)
+    res.status(200).json({'message': 'test_example_local'})
   }
 )
 
